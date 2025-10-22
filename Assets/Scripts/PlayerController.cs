9317622ea64196a969bee1d92d587e9507ab13e8
filipporomanello <romanellofilippo@icloud.com>
2025-10-22 13:29:17 +0200

    Unity's Input System

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    InputAction moveAction;
+    void Start()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+    }
+
+    void Update()
+    {
+        Vector2 moveVector;
+        moveVector = moveAction.ReadValue<Vector2>();
+    }
+}
