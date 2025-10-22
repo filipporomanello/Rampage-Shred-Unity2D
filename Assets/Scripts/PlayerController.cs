fdb83a0a02e0d0843472363b7bc52eac5eb7fe48
filipporomanello <romanellofilippo@icloud.com>
2025-10-22 15:58:06 +0200

    Rotating our player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,15 +3,27 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] float torqueAmount = 5f;
+
     InputAction moveAction;
+    Rigidbody2D myRigibody2D;
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
+        myRigibody2D = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         Vector2 moveVector;
         moveVector = moveAction.ReadValue<Vector2>();
+        if (moveVector.x < 0)
+        {
+            myRigibody2D.AddTorque(torqueAmount);
+        }
+        else if (moveVector.x > 0)
+        {
+            myRigibody2D.AddTorque(-torqueAmount);
+        }
     }
 }
