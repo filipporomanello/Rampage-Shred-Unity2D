53cfbc3f2773bbfafd842ca491883bb6d3356870
filipporomanello <romanellofilippo@icloud.com>
2025-10-23 11:49:04 +0200

    fix

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float torqueAmount = 2f;
+    [SerializeField] float torqueAmount = 1f;
 
     InputAction moveAction;
     Rigidbody2D myRigibody2D;
